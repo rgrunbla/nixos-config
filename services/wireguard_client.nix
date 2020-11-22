3596966100c2d5fd21b961e25fb3249a30187045
Rémy Grünblatt <remy@grunblatt.org>
2020-11-22 21:05:47 +0100

    Ipv6 for Wireguard

--- services/wireguard_client.nix
+++ services/wireguard_client.nix
@@ -6,7 +6,7 @@
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     wg0 = {
       # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "10.13.37.2/24" ];
+      ips = [ "10.13.37.2/24" "2001:41d0:0001:f45e:8000::2/65" ];
 
       # Path to the private key file.
       privateKeyFile = "${../nixos-secrets/wireguard_sauron_private.key}";
@@ -19,7 +19,7 @@
           # Public key of the server (not a file path).
           publicKey = "iqk2Yxf8aKi6vTRsDMOuwAnOGtPGLIZEIUNegyMSgn0=";
 
-          allowedIPs = [ "0.0.0.0/0" ];
+          allowedIPs = [ "0.0.0.0/0" "::/0" ];
 
           # Set this to the server IP and port.
           endpoint = "91.121.179.94:51820";
--- services/wireguard_server.nix
+++ services/wireguard_server.nix
@@ -27,7 +27,7 @@
         interfaces = {
           wg0 = {
             # Determines the IP address and subnet of the server's end of the tunnel interface.
-            ips = [ "10.13.37.1/24" ];
+            ips = [ "10.13.37.1/24" "2001:41d0:0001:f45e:8000::1" ];
 
             # The port that Wireguard listens to. Must be accessible by the client.
             listenPort = 51820;
@@ -35,12 +35,18 @@
             # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
             # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
             postSetup = ''
+              ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
               ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.13.37.0/24 -o eth0 -j MASQUERADE
+              ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
+              ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s 2001:41d0:0001:f45e:8000::1/65 -o eth0 -j MASQUERADE
             '';
 
             # This undoes the above command
             postShutdown = ''
+              ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
               ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.13.37.0/24 -o eth0 -j MASQUERADE
+              ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
+              ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s 2001:41d0:0001:f45e:8000::1/65 -o eth0 -j MASQUERADE
             '';
 
             # Path to the private key file.
@@ -57,7 +63,7 @@
                 # Public key of the peer (not a file path).
                 publicKey = "4xcf/dLyOix/B7ME9XpbWIbDbF4x4+E30dc1vIm7dRY=";
                 # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
-                allowedIPs = [ "10.13.37.2/32" ];
+                allowedIPs = [ "10.13.37.2/32" "2001:41d0:0001:f45e:8000::2/128" ];
               }
             ];
           };
